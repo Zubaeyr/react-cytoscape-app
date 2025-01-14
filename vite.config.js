4cc675a89942626500c8a4e2bd09491b15613e44
Zubaeyr <wisestofall@gmail.com>
2025-01-14 16:21:54 +0530

    updating vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // Specify the port for development
+  },  
   build: {
     outDir: 'dist', // Output directory for the build
     sourcemap: true, // Generate source maps for debugging
