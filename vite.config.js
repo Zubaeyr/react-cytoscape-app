e699aa0cb482abcbbd9bafec97d2407d697cf61f
Zubaeyr <wisestofall@gmail.com>
2025-01-14 16:01:07 +0530

    updating startup script

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for the build
+    sourcemap: true, // Generate source maps for debugging
+  },
 })
